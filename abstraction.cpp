5ba08a44ea06849531c10aaea1841a9868f503ac
modiparshva <parshvamodi2005@gmail.com>
2025-05-14 21:04:40 +0530

    Abstraction With Car !

---
+++ abstraction.cpp
@@ -0,0 +1,82 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+class Car {
+    public:
+        virtual void startEngine() = 0;
+        virtual void shiftGear(int gear) = 0;
+        virtual void accelerate() = 0;
+        virtual void brake() = 0;
+        virtual void stopEngine() = 0;
+        virtual ~Car() {}
+};
+
+class sportsCar : public Car{
+    public:
+        string brand;
+        string model;
+        bool isEngineOn;
+        int currentSpeed;
+        int currentGear;
+
+        sportsCar(string b,string m){
+            this->brand = b;
+            this->model = m;
+            isEngineOn = false;
+            currentSpeed = 0;
+            currentGear = 0;
+        }
+
+        void startEngine() {
+            isEngineOn = true;
+            cout << brand << " " << model << " : Engine Started ! " << endl; 
+        }
+
+        void shiftGear(int gear) {
+            if (!isEngineOn) {
+                cout << brand << " " << model << " : Engine is not started. Please start the engine first." << endl;
+            }  
+            currentGear = gear;
+            cout << brand << " " << model << " : shifted to gear " << currentGear << endl;
+        }
+
+        void accelerate() {
+            if (!isEngineOn) {
+                cout << brand << " " << model << " : Engine is not started. Please start the engine first." << endl;
+            }
+
+            currentSpeed += 20;
+            cout << brand << " " << model << " : Accelerating at " << currentSpeed << endl;
+        }
+
+        void brake() {
+            if (!isEngineOn) {
+                cout << brand << " " << model << " : Engine is not started. Please start the engine first." << endl;
+            }
+            currentSpeed -= 10;
+            cout << brand << " " << model << " : Braking at " << currentSpeed << endl;
+        }
+
+        void stopEngine() {
+            if (!isEngineOn) {
+                cout << brand << " " << model << " : Engine is already stopped." << endl;
+            }
+            isEngineOn = false;
+            cout << brand << " " << model << " : Engine stopped !" << endl;
+        }
+};
+
+int main(){
+    Car* myCar = new sportsCar("Ford","Mustang");
+    myCar->startEngine();
+    myCar->shiftGear(1);
+    myCar->accelerate();
+    myCar->brake();
+    myCar->stopEngine();
+    myCar->shiftGear(0);
+
+    delete myCar;
+
+    return 0;
+}
